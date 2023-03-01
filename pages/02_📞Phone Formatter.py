e9711c2b06542b46034c1f586034bbc072487411
Kamal Thampi <khamsa.kamal48@gmail.com>
2023-03-01 18:47:31 +0530

    Removed buggy clipboard code

--- pages/02_📞Phone Formatter.py
+++ pages/02_📞Phone Formatter.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import re
-# import pyperclip
 import pandas as pd
 
 st.set_page_config(
@@ -16,10 +15,6 @@ hide_streamlit_style = """
             """            
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-# def copy_to_clipboard(text):
-#     pyperclip.copy(text)
-#     st.success("Copied to clipboard!")
-
 st.title("Phone Number Formatter")
 
 phone_number = st.text_input("Enter the phone number")
@@ -28,7 +23,6 @@ phone_number = re.sub("[^0-9]", "", phone_number)
 st.write("<br>", unsafe_allow_html=True)
 
 st.write("Formatted phone number:")
-# st.write(f"<h2 style='font-family: Arial;'>{phone_number}</h2>", unsafe_allow_html=True)
 
 st.code(phone_number)
 
