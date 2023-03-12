b8d2c3e5cf93910368db64580d47492128689b1c
Kamal Thampi <khamsa.kamal48@gmail.com>
2023-03-12 23:54:05 +0530

    Corrected to ensure only email address is used

--- pages/03_🔍Search RE ID.py
+++ pages/03_🔍Search RE ID.py
@@ -23,19 +23,19 @@ def get_data():
 data = get_data()
 
 # Define the Streamlit app
-st.title("Search for ID by contact details")
+st.title("Search for ID by Email address")
 
 # Add a search box for email
-search_box = st.text_input("Enter contact detail to search:")
+search_box = st.text_input("Enter email address to search:")
 
 # Filter the dataframe by the entered email and show the corresponding ID
 if search_box:
     result = data.loc[data['address'].str.strip().str.match(search_box.strip(),case=False), 'constituent_id']
 
     if len(result.unique()) == 1:
-        st.write("The ID for the entered contact is:")
+        st.write("The ID for the entered email address is:")
 
         st.code(result.values[0])
         
     else:
-        st.write("No ID found for the entered contact. Kindly check manually in Raisers Edge.")
+        st.write("No ID found for the entered email. Kindly check manually in Raisers Edge.")
